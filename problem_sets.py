98a26e2a89e7069019c3cafe61b31af28fd53b6d
Kotek - Linux <kotek.vojtech@gmail.com>
2016-01-25 18:02:22 +0100

    finalized ps1

--- problem_sets.py
+++ problem_sets.py
@@ -1,9 +1,8 @@
-word = str(input("Input word:"))
 vowels_count = 0
 vowels = ['a','e','i','o','u']
 
-for letter in word:
+for letter in s:
     if letter in vowels:
         vowels_count+=1
 
-print("There are %d in the word '%s'") % (vowels_count, word)
+print("Number of vowels: ", vowels_count)
