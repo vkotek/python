1f3f785c2db1a6f17c7271ce6816259ee6abb95c
Kotek - Linux <kotek.vojtech@gmail.com>
2016-01-25 22:18:50 +0100

    finalized bob counter

--- problem_sets.py
+++ problem_sets.py
@@ -1,8 +1,25 @@
-vowels_count = 0
-vowels = ['a','e','i','o','u']
 
-for letter in s:
-    if letter in vowels:
-        vowels_count+=1
+# PS1, EX1
+def count_vowels(s):
+    vowels_count = 0
+    vowels = ['a','e','i','o','u']
 
-print("Number of vowels: ", vowels_count)
+    for letter in s:
+        if letter in vowels:
+            vowels_count+=1
+
+    print("Number of vowels: ", vowels_count)
+
+# PS1, EX2
+def count_bobs(s):
+    bobs = 0
+    for x in range(0,len(s)):
+        if s[x:x+3] == 'bob':
+            bobs +=1
+    return bobs
+    
+def odd(x):
+    return x % 2 == 0
+    
+
+print(count_bobs("Hellobobobral"))
