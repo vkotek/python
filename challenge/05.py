cbc26d81ad25ac0b9cd7e57f71d9cef53a2d9001
Vojtech Kotek <kotek.vojtech@gmail.com>
2016-12-31 00:14:17 +0100

    ch06 complete

--- challenge/05.py
+++ challenge/05.py
@@ -8,7 +8,6 @@ fl = "/Users/ares/Documents/Scripts/python/challenge/05.p"
 with open(fl, 'rb') as f:
     data = pickle.load(f)
 
-
 for row in data:
     for item in row:
         print(item[0]*item[1], end="")
